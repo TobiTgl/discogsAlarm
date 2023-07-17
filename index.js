469b137f67bba96bff7cc9fd30e215db8bbb2ddd
TobiTgl <tobias_813@t-online.de>
2023-07-17 12:50:00 +0200

    curl converted node axios fetch

--- index.js
+++ index.js
@@ -122,16 +122,28 @@ const checkDoclibDatesBlaustein = ()=> {
         method: 'get',
         url: 'https://www.doctolib.de/availabilities.json?visit_motive_ids=4322286&agenda_ids=476079-476080&practice_ids=187855&insurance_sector=private&telehealth=false&limit=5&start_date='+slicedeDate,
         headers: {
-            'Content-Type': 'text/plain',
-            'Cookie': '__cf_bm=575tiWlYlpWZnloowgam1rZlQdNgN0YpswkCsjSn6uw-1689588661-0-AaDYtjyKTweSieIGbzyLs2UA6E5p4i422+jG6AJrRmH2gi214JDW8VJP9p8VHWtcLJKvWynVwVHUxgPI6LJqZKo7hFGEmje/rdlCSjtH9J7k;ssid=ub00unk-nLAXiFG3oFN6; path=/; expires=Sat, 17 Aug 2024 10:13:01 GMT; secure;esid=-u1aoPLxAnJiYJxrTM_d4Aoc; path=/; secure;_doctolib_session=InVW%2FLkJNMLUfZJGvE%2FrDQiCjgCLnoCndF9mbyuALMnUbjBASkcLTF1Ise9u%2BBUqzork016AOADK929MB3lnjH1BL1Y8m9T6U%2FtO2cKVtbFIvMDiL6nwPWcSajBvVJENoc2bQTVPMqqsxqzeu99ynu5tpHj42fAD5FptcHntcjQUizBkudGs3zsRjk1ZK7Vu1mw8kmcjQKwr5af8GWcyMYfnXX0d2SyB%2BMYnd%2BcvG8Sz0qeHsSLPdOMwa7VGqF1qff1uacFHaHCSEF5pfvPxXjuVWc5aunB96gn4IdnDt7AwAQpIeN0lv3lIbinDcv6bbJSXexvQu8jzeJDQvCzZdcnAcIsDsRL8wKRNACrsGQ40dKuCFsX%2FORMC2BTpCtWPiGs5Ns4ogHTLo0BLLjsps4ob3NxGRRc8w3tZ7RtIFk28qMxr4BSb%2Faw%3D--Q9J1lK0Bm9rVAUS7--SkB8kWhpVeMRdvE2ZpAVzw%3D%3D',
+            'authority': 'www.doctolib.de',
+            'accept': 'application/json',
+            'accept-language': 'de-DE,de;q=0.9',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json; charset=utf-8',
+            'cookie': 'ssid=c114000win-0MhNAJpcDpqI; esid=WkUUGaU649PrfnikPlVcyCk1; __cf_bm=iamWlLasY304Eq48.JyPFvIV1.r_I.m4F2cdTbDdZTg-1689589603-0-Ac3ILCjdcB+VgvtzWAGVh/bqjUehJG3UVRUGvLzELlZLNYL/LqrRnc4gh1F4nLpxPOalRQJVpSE8HM/gZQbimZWrdPQCxjjVPiMmrZDtYPFu; didomi_token=eyJ1c2VyX2lkIjoiMTg5NjM2MzUtODFhMy02MzhlLTgyN2YtYzQ4Zjk1NmM4OWM5IiwiY3JlYXRlZCI6IjIwMjMtMDctMTdUMTA6MjY6NDQuMjU0WiIsInVwZGF0ZWQiOiIyMDIzLTA3LTE3VDEwOjI2OjQ0LjI1NFoiLCJ2ZW5kb3JzIjp7ImRpc2FibGVkIjpbImM6ZGlkb21pLWZZUEJZeFdhIiwiYzpkb2N0b2xpYm4tV3A4N0NwWEEiLCJjOmRvY3RvbGliYS10Z3RiM1c4UCJdfSwicHVycG9zZXMiOnsiZGlzYWJsZWQiOlsiYW5hbHl0aWNzLU5HcXhXYm1uIiwiYW5hbHl0aWstTjJaSDlCcVEiLCJkaXNwbGF5dGEtVjhrTWVuWWEiLCJkaXNwbGF5dGEtVnJQUFZuSGgiXX0sInZlcnNpb24iOjJ9; euconsent-v2=CPvDS0APvDS0AAHABBENDNCgAAAAAAAAAAAAAAAAAAAA.YAAAAAAAAAAA; _doctolib_session=9W%2BVF2bdjI6ncuTejZJc6T47caXRTN6hgVFVQv9cp1zyyhNK7vvi1TV9BE7olZ6og4hjAq9zdPmm7nZyjEV1GKBj378kJAg5SYcrJInf6Svc1t1V6e07QC%2B%2Fev7vboJ9rT4QbyTbsRKYLN%2B2cvHC3ApSakuUaE8o9U1BbneqUvL3vn3laHyJ6daIUyuv%2FVFy0XPmiMnj30zqQCJMRGhjWq5wjZXrpN3AT0LEKgTlXaiVNEwcC7FhCPl%2FVPByThBc61Ghw6NKrKtDWRsCWls1%2F6uVs1EsercUbqgTtUnyBx%2BII1fX5MJR6t%2BJJyXzgzcseOvqM6t96iuErKJkRGCEDy00mSkrcfoSuxfKbQQw4tHjVgu7j7cW1a48bM2AzFfDHXInm4LTejE8dY%2BljKuNrqxX72XRPgX6Z7USn22DlzyBCHEyn3i5VNE%3D--oOWT01yHMqjUs7pb--TG0FtqoQFAfTetHh8BSIwA%3D%3D',
+            'pragma': 'no-cache',
+            'referer': 'https://www.doctolib.de/medizinisches-versorgungszentrum-mvz/blaustein/blausteinklinik-hautzentrum-ulm/booking/availabilities?specialityId=1289&telehealth=false&placeId=practice-187855&insuranceSector=public&insuranceSectorEnabled=true&motiveCategoryIds%5B%5D=141299&motiveIds%5B%5D=4322286&practitionerId=NO_PREFERENCE',
+            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            'x-csrf-token': 'Bcqqcco7lDeY2t3sk9yj6IBVk7qdKoVC3qpeUFP8GltBwN4atSmB/1lsw61Hv3vDSOXpKnUaLfjfg0MU8TLJ6A=='
         }
     };
 
     axios(configDoclib)
         .then(function (response) {
-            console.log(response)
             if (response.data.next_slot != undefined) {
-                console.log(response.data.next_slot);
                 sendTelegramMessage('BlausteinKlinik next slot: ' + response.data.next_slot);
             }
         })
